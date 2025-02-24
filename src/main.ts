c2db9caa605c713dca2c83a96fa731fc67a2b639
Abstractmelon <abstractmelons@gmail.com>
2025-02-24 12:57:22 -0700

    EVERYBODY GET THE THEME OUT!!

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/base.css'
-import './assets/styles/themes.css'
 
 const app = createApp(App)
 const pinia = createPinia()
